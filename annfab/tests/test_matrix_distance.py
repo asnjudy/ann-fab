3671a401ac7164bd14758b94a9085fbb192b8671
Evan Lezar <evan.lezar@zalando.de>
2015-12-14 16:48:23 +0100

    Add simple tests for matrix operands for distance measure.

--- annfab/tests/test_matrix_distance.py
+++ annfab/tests/test_matrix_distance.py
@@ -30,7 +30,7 @@ def test_matrix_distance_y_is_matrix():
     x = np.random.randn(n)
     y0 = np.random.randn(n)
 
-    y = np.empty((n,m))
+    y = np.empty((n, m))
 
     for i in xrange(m):
         y[:, i] = y0
@@ -45,3 +45,54 @@ def test_matrix_distance_y_is_matrix():
 
     for i in xrange(m):
         assert md[i] == vd
+
+
+def test_matrix_distance_x_is_matrix():
+    n = 100
+    k = 5
+    x0 = np.random.randn(n)
+    y = np.random.randn(n)
+
+    x = np.empty((n, k))
+    for i in xrange(k):
+        x[:, i] = x0
+
+    vD = nearpy.distances.CosineDistance()
+    vd = vD.distance(x0, y)
+
+    mD = annfab.distances.MatrixCosineDistance()
+    md = mD.distance(x, y)
+
+    assert len(md) == k
+
+    for i in xrange(k):
+        assert md[i] == vd
+
+
+def test_matrix_distance_x_and_y_are_matrices():
+    n = 100
+    m = 10
+    k = 5
+    x0 = np.random.randn(n)
+    y0 = np.random.randn(n)
+
+    y = np.empty((n, m))
+    x = np.empty((n, k))
+
+    for i in xrange(m):
+        y[:, i] = y0
+
+    for i in xrange(k):
+        x[:, i] = x0
+
+    vD = nearpy.distances.CosineDistance()
+    vd = vD.distance(x0, y0)
+
+    mD = annfab.distances.MatrixCosineDistance()
+    md = mD.distance(x, y)
+
+    assert md.shape == (k, m)
+
+    for j in xrange(k):
+        for i in xrange(m):
+            assert md[j, i] == vd
