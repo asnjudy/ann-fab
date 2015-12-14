0c69c3e25f52ab126886a2a2f1c33d099d0bb5a4
Evan Lezar <evan.lezar@zalando.de>
2015-12-14 17:25:49 +0100

    Add simple tests for matrix operands for distance measure.

--- annfab/distances/matrix_cosine.py
+++ annfab/distances/matrix_cosine.py
@@ -9,18 +9,17 @@ class MatrixCosineDistance(nearpy.distances.CosineDistance):
     """
 
     def distance(self, x, y):
-    	if len(x.shape) <= 1:
-    		return super(MatrixCosineDistance, self).distance(x, y)
+        if len(x.shape) <= 1:
+            return super(MatrixCosineDistance, self).distance(x, y)
 
-    	k = x.shape[1]
-    	if len(y.shape) > 1:
-    		m = y.shape[1]
-    	else:
-    		m = 1
+        k = x.shape[1]
+        if len(y.shape) > 1:
+            m = y.shape[1]
+        else:
+            m = 1
 
-    	d = np.empty((k, m), dtype=np.dtype(x[0,0]))
-    	for i in xrange(k):
-    		d[i,:] = super(MatrixCosineDistance, self).distance(x[:,i], y)
+        d = np.empty((k, m), dtype=np.dtype(x[0, 0]))
+        for i in xrange(k):
+            d[i, :] = super(MatrixCosineDistance, self).distance(x[:, i], y)
 
-    	return d
-
+        return d
